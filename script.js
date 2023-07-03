e9fc4e21b97845c13f3f154b094692663d69fb45
arch0root <68467576+init0root@users.noreply.github.com>
2023-07-02 21:18:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-var btn = document.getElementById("countdown")
-var segundos = 300;
-var minutos = 4;
-var interval = setInterval(count, 1000)
-function count(){
-	segundos = segundos - 1;
-	btn.innerText = segundos;
-	// if(segundos >= 10){
-	// 	btn.innerText = minutos + ":" + segundos;
-	// }else{ btn.innerText = minutos + ":" + "0" + segundos;}
-	if(segundos == 0 && minutos == 0){
-		segundos, minutos = 0;
-		clearInterval(interval)
-	}
-	if(segundos == 0 && minutos >= 0){
-		segundos = 60;
-		minutos = minutos - 1;
-	}
+var counter
+
+var topics = [{
+	"ep": "EPISÓDIO 2",
+	"title":"O QUE É UMA DESGRAÇA?",
+	"text":"O QUE É UMA CABRUNCA",
+	"topic":"p1"
+},
+
+{
+	"ep":"EPISÓDIO 2",
+	"title":"TESTANDO O TITULO",
+	"topic":"p2"
 }
 
+]
 
-function test(){
-    clearInterval(interval)
+
+
+
+function start(){
+	var tst = document.getElementById(topics[0].topic)
+	tst.style.visibility = "visible"
+	topics.shift()
 }
-
 
 function closeModal(modal){
+	console.log(topics[0].ep)
+	var idEp = document.getElementById("ep").innerText = topics[0].ep
+	var epText = document.getElementById("titleEP").innerText = topics[0].title
 	modal.parentElement.style.visibility = 'hidden'
+
 
 }
 
-function modalVisible(id){
-	console.log(document.getElementById(id))
-	document.getElementById(id).style.visibility = "visible"
+function startTopic(){
+
 }
