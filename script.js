9d9631f7a9e5b204a798ac785550ad8db10ff9b5
arch0root <68467576+init0root@users.noreply.github.com>
2023-07-04 10:45:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,53 @@ var counter
 
 var topics = [{
 	"ep": "EPISÓDIO 2",
-	"title":"O QUE É UMA DESGRAÇA?",
+	"title":"O QUE É UMA NAÇÃO SEM TERRITÓRIO?",
 	"text":"O QUE É UMA CABRUNCA",
 	"topic":"p1"
 },
 
 {
 	"ep":"EPISÓDIO 2",
-	"title":"TESTANDO O TITULO",
-	"topic":"p2"
+	"title":"O QUE É UMA NAÇÃO SEM TERRITÓRIO?",
+	"topic":"p2",
+	"image":"t2.png"
+},
+
+{
+	"ep":"EPISÓDIO 3",
+	"title":"O QUE É A CHECHÊNIA?",
+	"topic":"p3",
+	"image":"t3.png"
 }
 
 ]
+
+
+var questions = [{
+	"repV" : "ru"
+},
+
+{
+	"repV": "ram"
+}
+
+]
+
+function quizz(rep){
+
+  if(rep.getAttribute("rep") == "ago"){
+  	alert("kkkkkkkkkk.")
+  }
+  if(rep.getAttribute("rep") == questions[parseInt(rep.getAttribute("sel"))].repV){
+  	rep.style="background-color: rgb(0,127,0,0.5);"
+  	clicked = 1;
+  }else{
+  	rep.style="background-color: rgb(255,0,0,0.5);"
+  	clicked = 1;
+  }
+ 
+
+}
 
 
 
@@ -24,11 +59,17 @@ function start(){
 	topics.shift()
 }
 
+
+
+
 function closeModal(modal){
 	console.log(topics[0].ep)
 	var idEp = document.getElementById("ep").innerText = topics[0].ep
 	var epText = document.getElementById("titleEP").innerText = topics[0].title
 	modal.parentElement.style.visibility = 'hidden'
+	var img = document.getElementsByClassName('presentation')[0].style = "background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 1.0)), url(" + topics[0].image + ");"
+	console.log(img.style)
+
 
 
 }
